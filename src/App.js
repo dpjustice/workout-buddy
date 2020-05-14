b51641c8d000da6d2789ad1acf3889bfcd8e1066
David Parsons <djp424@gmail.com>
2020-05-14 19:16:25 -0400

    v2 of app with no promices

--- src/App.js
+++ src/App.js
@@ -3,22 +3,12 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  // var startTime
-  // var endTime
-  var myInterval = false
-  // var setPromices
-  var timeLeft
+  var workoutInterval = false
 
   function playSound(frequency = 261.6) {
-    // console.log('play sound')
-    // console.log(message)
-    // endTime = new Date()
-    // let totalTime = endTime - startTime
-    // console.log(Math.round(totalTime/1000))
-
-    var context = new AudioContext()
-    var o = context.createOscillator()
-    var g = context.createGain()
+    let context = new AudioContext()
+    let o = context.createOscillator()
+    let g = context.createGain()
     o.connect(g)
     g.connect(context.destination)
     g.gain.exponentialRampToValueAtTime(
@@ -29,44 +19,75 @@ function App() {
     o.start(0)
   }
 
-  function countdown(ms) {
-    let timer = document.getElementById("timer")
-    timeLeft = Math.round(ms/1000)
-    timer.innerHTML = String(timeLeft)
+  function startTimer(workoutTime, restTime, sets) {
+    // Clear out old countdown to ensure no duplication.
+    if (workoutInterval !== false) {
+      clearInterval(workoutInterval)
+    }
 
-    // Clear out old countdown.
-    if (myInterval !== false) {
-      clearInterval(myInterval)
+    // Add alarm times to arrays.
+    let timer = document.getElementById("timer")
+    let background = document.querySelector(".App-header")
+    let restAlarms = [0]
+    let workoutAlarms = []
+    let totalTime = 0
+    let timeLeft = restTime
+    let currentTime = 0
+    for (let set = 0; set < sets; set++) {
+      workoutAlarms.push(totalTime + restTime)
+      restAlarms.push(totalTime + restTime + workoutTime)
+      totalTime += restTime + workoutTime
+    }
+    // console.log(workoutAlarms)
+    // console.log(restAlarms)
+
+    function updateTimer() {
+      // console.log('-------')
+      // console.log(currentTime)
+      // console.log(timeLeft)
+
+      // Workout actions.
+      if (workoutAlarms.includes(currentTime)) {
+        // console.log('WORKOUT SOUND')
+        playSound()
+        timeLeft = workoutTime
+        background.style.background = '#4169E1'
+      }
+
+      // Rest actions.
+      if (restAlarms.includes(currentTime)) {
+        // console.log('REST SOUND')
+        playSound()
+        timeLeft = restTime
+        background.style.background = '#666600'
+      }
+
+      // Display time left.
+      timer.innerHTML = String(timeLeft)
+
+      // Update times for next check.
+      currentTime += 1
+      timeLeft -= 1
+
+      // Clear interval if needed.
+      if (currentTime > totalTime || timeLeft < 0) {
+        clearInterval(workoutInterval)
+        timer.innerHTML = "Workout Complete"
+        background.style.background = '#228B22'
+        document.getElementById("button").disabled = false
+      }
     }
 
     // Set new coutdown.
-    myInterval = setInterval(() => {
-      timeLeft -= 1
-      if ( timeLeft >= 0 ) {
-        timer.innerHTML = String(timeLeft)
-      }
-      if ( timeLeft <= 0 ) {
-        clearInterval(myInterval)
-      }
-    }, 1000); // Run every second.
-
-    return new Promise(resolve => setTimeout(resolve, ms));
-  }
-
-  function runTimer(time, rest, setsLeft) {
-    if (setsLeft > 0) {
-      countdown(time*1000).then(() => playSound(261.6))
-        .then(() => countdown(rest*1000)).then(() => playSound(261.6))
-        .then(() => countdown(120)).then(() => playSound(261.6))
-        .then(() => runTimer(time, rest, setsLeft - 1))
-    } else {
-      document.getElementById("button").disabled = false
-    }
+    updateTimer() // Hack to run timer at the first second.
+    workoutInterval = setInterval(updateTimer, 1000); // Run every second.
   }
 
   function handleClick(e) {
     e.preventDefault();
-    // startTime = new Date()
+
+    // startTimer(5, 3, 3)
+    // return
 
     // Button.
     document.getElementById("button").disabled = true
@@ -76,25 +97,23 @@ function App() {
     let rest = parseInt(document.getElementById("rest").value)
     let sets = parseInt(document.getElementById("sets").value)
 
-    console.log(time)
+    // console.log(time)
 
     if ( isNaN(time) || isNaN(rest) || isNaN(sets) || time <= 0 || rest <= 0 || sets <= 0 ) {
       document.getElementById("button").disabled = false
       return
     }
 
-    countdown(10000) // 10 seconds pass before starting.
-      .then(() => playSound) // Start workout notification
-      .then(() => runTimer(time, rest, sets))
+    startTimer(time, rest, sets)
   }
 
   return (
     <div className="App">
       <header className="App-header">
         <div id="timer">Timer</div>
-        <input id="time" class="inputNumber" type="number" min="0" step="1" required placeholder="Rep Time (sec)"></input>
-        <input id="rest" class="inputNumber" type="number" min="0" step="1" required placeholder="Rest Time (sec)"></input>
-        <input id="sets" class="inputNumber" type="number" min="0" step="1" required placeholder="How many Sets?"></input>
+        <input id="time" className="inputNumber" type="number" min="0" step="1" required placeholder="Rep Time (sec)"></input>
+        <input id="rest" className="inputNumber" type="number" min="0" step="1" required placeholder="Rest Time (sec)"></input>
+        <input id="sets" className="inputNumber" type="number" min="0" step="1" required placeholder="How many Sets?"></input>
         <button id="button" onClick={handleClick}>Run Timer</button>
       </header>
     </div>
